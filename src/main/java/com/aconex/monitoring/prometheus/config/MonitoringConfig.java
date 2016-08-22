ea1b8a23d73f28c8ce1ad3229d8866f8cb25fd34
Xiaojun REN <nicholas.x.ren@gmail.com>
2016-08-22 20:50:33 +1000

    Registered RequestStatsCollector as a bean.

--- src/main/java/com/aconex/monitoring/prometheus/config/MonitoringConfig.java
+++ src/main/java/com/aconex/monitoring/prometheus/config/MonitoringConfig.java
@@ -2,9 +2,11 @@ package com.aconex.monitoring.prometheus.config;
 
 import javax.annotation.PostConstruct;
 
+import com.aconex.monitoring.prometheus.collector.RequestStatsCollector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,15 @@ public class MonitoringConfig {
     @Bean
     public ServletRegistrationBean registerPrometheusExporterServlet() {
         return new ServletRegistrationBean(new MetricsServlet(CollectorRegistry.defaultRegistry), "/metrics");
+    }
+
+    @Bean
+    public FilterRegistrationBean requestStatsCollector(String urlPattern, int order) {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new RequestStatsCollector());
+        bean.addUrlPatterns(urlPattern);
+        bean.setOrder(order);
+        return bean;
     }
 
     @PostConstruct
