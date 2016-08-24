8d3f178da533ad35f64d190100ab57c063eaa60a
Xiaojun REN <nicholas.x.ren@gmail.com>
2016-08-25 09:45:46 +1000

    integration tests

--- sample-app/src/main/java/com/example/Application.java
+++ sample-app/src/main/java/com/example/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import(MonitoringConfig.class) //TODO: import JavaConfig
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
