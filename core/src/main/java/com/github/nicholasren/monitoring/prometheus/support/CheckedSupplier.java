a4463a2804f67ba0a341e5a51a6c49d57e6090f8
Xiaojun REN <nicholas.x.ren@gmail.com>
2016-08-26 10:38:29 +1000

    fix deployment issue

--- core/src/main/java/com/github/nicholasren/monitoring/prometheus/support/CheckedSupplier.java
+++ core/src/main/java/com/github/nicholasren/monitoring/prometheus/support/CheckedSupplier.java
@@ -3,7 +3,7 @@ package com.github.nicholasren.monitoring.prometheus.support;
 /**
  * Wrapper of a supplier which may throw `Throwable`
  *
- * @param <T>
+ * @param <T>  type of supplied value.
  */
 @FunctionalInterface
 public interface CheckedSupplier<T> {
