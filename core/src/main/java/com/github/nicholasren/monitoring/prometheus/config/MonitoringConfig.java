2675bfa3c3bf7a3f137dc136194030a5c57cec8d
Xiaojun REN <nicholas.x.ren@gmail.com>
2016-09-26 12:36:18 +1000

    Change filter order of request stats collector and

--- core/src/main/java/com/github/nicholasren/monitoring/prometheus/config/MonitoringConfig.java
+++ core/src/main/java/com/github/nicholasren/monitoring/prometheus/config/MonitoringConfig.java
@@ -3,8 +3,8 @@ package com.github.nicholasren.monitoring.prometheus.config;
 import javax.annotation.PostConstruct;
 
 import com.github.nicholasren.monitoring.prometheus.collector.RequestStatsCollector;
-import com.github.nicholasren.monitoring.prometheus.collector.TransactionLatencyCollector;
 import com.github.nicholasren.monitoring.prometheus.collector.TransactionLatencyAspect;
+import com.github.nicholasren.monitoring.prometheus.collector.TransactionLatencyCollector;
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.DefaultExports;
@@ -15,6 +15,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MonitoringConfig {
+
+    static final int REQUEST_STATS_COLLECTOR_ORDER = 1;
+
     @Bean
     public ServletRegistrationBean registerPrometheusExporterServlet() {
         return new ServletRegistrationBean(new MetricsServlet(CollectorRegistry.defaultRegistry), "/metrics");
@@ -22,7 +25,7 @@ public class MonitoringConfig {
 
     @Bean
     public FilterRegistrationBean requestStatsCollector() {
-        return RequestStatsCollector.create("/*", 100);
+        return RequestStatsCollector.create("/*", REQUEST_STATS_COLLECTOR_ORDER);
     }
 
     @Bean
